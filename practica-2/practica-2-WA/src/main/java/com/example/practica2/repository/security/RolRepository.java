be6d186f65fdb52b7616225e1342150df043ea10
Hiroshy <HBLL0001@ce.pucmm.edu.do>
2026-03-22 00:10:32 -0300

    arreglos de tareas

--- practica-2/practica-2-WA/src/main/java/com/example/practica2/repository/security/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practica2.repository.security;
-
-import com.example.practica2.encapsulators.security.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol, String> {
-
-    Rol findByRole(String rol);
-}
